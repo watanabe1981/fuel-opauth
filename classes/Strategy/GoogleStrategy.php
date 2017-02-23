eec2425185c5617eddb96e84ea7435af45668546
渡邉勇 <i.watanabe@funaffect.jp>
2017-02-23 17:21:37 +0900

    [update] OAuth Version 2.0 update.

--- classes/Strategy/GoogleStrategy.php
+++ classes/Strategy/GoogleStrategy.php
@@ -1,10 +1,18 @@
 <?php
 /**
  * Google strategy for Opauth
+ *
+ * @copyright    Copyright 2017 Isamu Watanabe (http://developpers.info)
+ * @package      Opauth.GoogleStrategy
+ * @see based on https://developers.google.com/identity/protocols/OpenIDConnect#setredirecturi
+ */
+
+/**
+ * Google strategy for Opauth
  * based on https://developers.google.com/accounts/docs/OAuth2
- * 
+ *
  * More information on Opauth: http://opauth.org
- * 
+ *
  * @copyright    Copyright © 2012 U-Zyn Chua (http://uzyn.com)
  * @link         http://opauth.org
  * @package      Opauth.GoogleStrategy
@@ -14,21 +22,21 @@
 /**
  * Google strategy for Opauth
  * based on https://developers.google.com/accounts/docs/OAuth2
- * 
+ *
  * @package			Opauth.Google
  */
 class GoogleStrategy extends OpauthStrategy{
-	
+
 	/**
 	 * Compulsory config keys, listed as unassociative arrays
 	 */
 	public $expects = array('client_id', 'client_secret');
-	
+
 	/**
 	 * Optional config keys, without predefining any default values.
 	 */
 	public $optionals = array('redirect_uri', 'scope', 'state', 'access_type', 'approval_prompt');
-	
+
 	/**
 	 * Optional config keys with respective default values, listed as associative arrays
 	 * eg. array('scope' => 'email');
@@ -37,26 +45,27 @@ class GoogleStrategy extends OpauthStrategy{
 		'redirect_uri' => '{complete_url_to_strategy}oauth2callback',
 		'scope' => 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
 	);
-	
+
 	/**
 	 * Auth request
 	 */
 	public function request(){
-		$url = 'https://accounts.google.com/o/oauth2/auth';
+		$url = 'https://accounts.google.com/o/oauth2/v2/auth';
 		$params = array(
-			'client_id' => $this->strategy['client_id'],
-			'redirect_uri' => $this->strategy['redirect_uri'],
+			'client_id'     => $this->strategy['client_id'],
+			'redirect_uri'  => $this->strategy['redirect_uri'],
 			'response_type' => 'code',
-			'scope' => $this->strategy['scope']
+			'scope'         => $this->strategy['scope'],
+			// 'state'
 		);
 
 		foreach ($this->optionals as $key){
 			if (!empty($this->strategy[$key])) $params[$key] = $this->strategy[$key];
 		}
-		
+
 		$this->clientGet($url, $params);
 	}
-	
+
 	/**
 	 * Internal callback, after OAuth
 	 */
@@ -72,12 +81,12 @@ class GoogleStrategy extends OpauthStrategy{
 				'grant_type' => 'authorization_code'
 			);
 			$response = $this->serverPost($url, $params, null, $headers);
-			
+
 			$results = json_decode($response);
-			
+
 			if (!empty($results) && !empty($results->access_token)){
 				$userinfo = $this->userinfo($results->access_token);
-				
+
 				$this->auth = array(
 					'uid' => $userinfo['id'],
 					'info' => array(),
@@ -92,13 +101,13 @@ class GoogleStrategy extends OpauthStrategy{
 				{
 					$this->auth['credentials']['refresh_token'] = $results->refresh_token;
 				}
-				
+
 				$this->mapProfile($userinfo, 'name', 'info.name');
 				$this->mapProfile($userinfo, 'email', 'info.email');
 				$this->mapProfile($userinfo, 'given_name', 'info.first_name');
 				$this->mapProfile($userinfo, 'family_name', 'info.last_name');
 				$this->mapProfile($userinfo, 'picture', 'info.image');
-				
+
 				$this->callback();
 			}
 			else{
@@ -119,15 +128,15 @@ class GoogleStrategy extends OpauthStrategy{
 				'code' => 'oauth2callback_error',
 				'raw' => $_GET
 			);
-			
+
 			$this->errorCallback($error);
 		}
 	}
-	
+
 	/**
 	 * Queries Google API for user info
 	 *
-	 * @param string $access_token 
+	 * @param string $access_token
 	 * @return array Parsed JSON results
 	 */
 	private function userinfo($access_token){
