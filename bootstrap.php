873e5177f9e432456676a841dc0307fcce304ff4
渡邉勇 <i.watanabe@funaffect.jp>
2017-02-23 12:23:08 +0900

    [add] YahooStrategy and GoogleStrategy

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Opauth Multi-provider authentication framework for PHP package for FuelPHP Framework
- * 
+ *
  * @package    Fuel-Opauth
  * @version    1.0
  * @author     Andreo Vieira <andreoav@gmail.com>
@@ -11,10 +11,12 @@
  */
 
 Autoloader::add_core_namespace('Opauth');
-  
+
 Autoloader::add_classes(array(
     'Opauth\\Opauth'           => __DIR__ . '/classes/Opauth.php',
     'Opauth\\OpauthStrategy'   => __DIR__ . '/classes/OpauthStrategy.php',
     'Opauth\\FacebookStrategy' => __DIR__ . '/classes/Strategy/FacebookStrategy.php',
     'Opauth\\TwitterStrategy'  => __DIR__ . '/classes/Strategy/TwitterStrategy.php',
+    'Opauth\\GoogleStrategy'   => __DIR__ . '/classes/Strategy/GoogleStrategy.php',
+    'Opauth\\YahooStrategy'    => __DIR__ . '/classes/Strategy/YahooStrategy.php',
 ));
