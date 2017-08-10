fea20c6f585470fa94d372b76080ff5353ee4685
渡邉勇 <i.watanabe@funaffect.jp>
2017-08-11 06:04:54 +0900

    add: lang

---
+++ lang/ja/oauth.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    // Oauth関連
+    'login-no-provider-specified'       => '不正な操作が行われました。',
+    'login-valid-settings'              => '設定が正しいか確認してください。',
+    'oauth.provider-linked'             => ':provider との紐付けを行いました。', // 関連付けが正常に行われたことをユーザに通知
+    'oauth.logged_in_using_provider'    => ':provider でログインしました。', // プロバイダを使用してログインが成功したことをユーザーに通知
+    'oauth.register-first'              => ':provider でログインしました。続けるにはサービスアカウントとの紐付けが必要です。', // プロバイダを使用してログインが成功したことをユーザーに通知、しかし、続けるにはローカルアカウントが必要
+    'oauth.auto-registered'             => 'ログインしました。', // プロバイダを使用してログインが成功したことをユーザーに通知、そして、ローカルアカウントが作成された
+    'oauth.canceled_your_authorisation' => '認証がキャンセルされました。再度お試しください。'
+];
